f15cf008dd6b984983f045d1fde9bfc3d8debcd3
germangrecoventura <85320986+germangrecoventura@users.noreply.github.com>
2022-09-18 21:58:08 -0300

    Add connection between API and React (#4)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,7 +6,18 @@ import io.javalin.apibuilder.ApiBuilder
 import io.javalin.http.NotFoundResponse
 
 fun main() {
-    val app = Javalin.create().start(7070)
+    val app = Javalin.create() {
+        it.defaultContentType = "application/json"
+        it.enableCorsForAllOrigins()
+    }.before {
+        it.header(io.javalin.core.util.Header.ACCESS_CONTROL_EXPOSE_HEADERS, "*")
+    }
+    app.start(7070)
+
+    app.before {
+        it.header("Access-Control-Expose-Headers", "*")
+    }
+
     app.routes {
         ApiBuilder.path("assistances") {
             ApiBuilder.get(AssistanceController(HibernateAssistanceDAO())::findAll)
