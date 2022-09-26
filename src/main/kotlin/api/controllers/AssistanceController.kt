14f40486496d11c97360f26a3e5d84b62e1544fc
germangrecoventura <85320986+germangrecoventura@users.noreply.github.com>
2022-09-25 21:58:42 -0300

    GAV-64: Agregar un parametro para filtrar asistencias por tipo (#7)
    * Add a parameter to filter assists by kind
    
    * Add parameter validator

--- src/main/kotlin/api/controllers/AssistanceController.kt
+++ src/main/kotlin/api/controllers/AssistanceController.kt
@@ -2,15 +2,31 @@ package api.controllers
 
 import api.dtos.ResultAssistanceDTO
 import dao.HibernateAssistanceDAO
+import io.javalin.http.BadRequestResponse
 import io.javalin.http.Context
+import io.javalin.http.NotFoundResponse
 import transaction.TransactionRunner.runTrx
 
 class AssistanceController(private val assistanceDAO: HibernateAssistanceDAO) {
     fun findAll(ctx: Context) {
-        val assistances = runTrx {
-            assistanceDAO.findAll()
+        if (ctx.queryParam("kind") == null) {
+            val assistances = runTrx {
+                assistanceDAO.findAll()
+            }
+            val result = ResultAssistanceDTO.fromModel(assistances)
+            ctx.json(result)
+        } else {
+            try {
+                val kind = ctx.queryParamAsClass("kind", String::class.java)
+                    .check({ c -> !c.isNullOrBlank() }, "No parameters added").get()
+                val assistances = runTrx {
+                    assistanceDAO.findAllByKind(kind)
+                }
+                val result = ResultAssistanceDTO.fromModel(assistances)
+                ctx.json(result)
+            } catch (e: BadRequestResponse) {
+                throw BadRequestResponse("No parameters added")
+            }
         }
-        val result = ResultAssistanceDTO.fromModel(assistances)
-        ctx.json(result)
     }
 }
--- src/test/kotlin/repository/HibernateDAOAssistantTest.kt
+++ src/test/kotlin/repository/HibernateDAOAssistantTest.kt
@@ -27,8 +27,8 @@ class HibernateDAOAssistantTest {
             "0303456664"
         )
 
-        val assistanceOne = Assistance(Kind.LARGE,250.0, 500.0, userOne)
-        val assistanceTwo = Assistance(Kind.MEDIUM, 325.0,425.0, userTwo)
+        val assistanceOne = Assistance(Kind.LARGE, 250.0, 500.0, userOne)
+        val assistanceTwo = Assistance(Kind.MEDIUM, 325.0, 425.0, userTwo)
 
         val assOne = runTrx {
             assistanceDAO.save(assistanceOne)
@@ -39,5 +39,51 @@ class HibernateDAOAssistantTest {
 
         Assertions.assertThat(userOne).isEqualTo(assOne.user)
         Assertions.assertThat(userTwo).isEqualTo(assTwo.user)
+    }
+
+    @Test
+    fun `wizard filtering by kind no data`() {
+        val assistanceDAO = HibernateAssistanceDAO()
+
+        val assistances = runTrx {
+            assistanceDAO.findAllByKind("SMALL")
+        }
+
+        Assertions.assertThat(assistances).isEmpty()
+    }
+
+    @Test
+    fun `wizard filtering by kind whiy data`() {
+        val assistanceDAO = HibernateAssistanceDAO()
+        val userOne = User(
+            "Test",
+            "McTest",
+            "ASSISTANCE",
+            "email@email.com",
+            "55556666"
+        )
+        val userTwo = User(
+            "German",
+            "McRonalds",
+            "ASSISTANCE",
+            "german@email.com",
+            "0303456664"
+        )
+
+        val assistanceOne = Assistance(Kind.LARGE, 250.0, 500.0, userOne)
+        val assistanceTwo = Assistance(Kind.SMALL, 325.0, 425.0, userTwo)
+
+        runTrx {
+            assistanceDAO.save(assistanceOne)
+        }
+        val assTwo = runTrx {
+            assistanceDAO.save(assistanceTwo)
+        }
+
+        val result = runTrx {
+            assistanceDAO.findAllByKind("SMALL")
+        }
+
+        Assertions.assertThat(result).contains(assTwo)
     }
 }
