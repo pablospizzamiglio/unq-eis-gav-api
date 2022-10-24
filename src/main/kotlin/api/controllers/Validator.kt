951a1df434c8c0a7a74663ba5571edf99268ec54
pablospizzamiglio <spizzamiglio.pablo@gmail.com>
2022-10-24 01:13:30 -0300

    Refactor validator

--- src/main/kotlin/api/controllers/Validator.kt
+++ src/main/kotlin/api/controllers/Validator.kt
@@ -5,21 +5,15 @@ import java.util.regex.Pattern
 
 class Validator {
     fun containsNumber(toCheck: String): Boolean {
-        var bool = false
-        for (element in toCheck) {
-            if (Character.isDigit(element)) {
-                bool = true
-            }
-        }
-        return bool
+        return toCheck.any { Character.isDigit(it) }
     }
 
     fun containsSpecialCharacter(str: String?): Boolean {
-        val regexCharacterSpecial = ("(?=.*[-+_!@#$%^&*.,?{}]).+$")
-        val special: Pattern = Pattern.compile(regexCharacterSpecial)
         if (str == null) {
             return false
         }
+        val regexCharacterSpecial = ("(?=.*[-+_!@#$%^&*.,?{}]).+$")
+        val special: Pattern = Pattern.compile(regexCharacterSpecial)
         val mSpecial: Matcher = special.matcher(str)
         return mSpecial.matches()
     }
