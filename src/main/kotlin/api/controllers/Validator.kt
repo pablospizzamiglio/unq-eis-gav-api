4d5388c0b17b7a81b84c3bca35a9d03f6568baf6
pablospizzamiglio <spizzamiglio.pablo@gmail.com>
2022-10-27 16:56:50 -0300

    Improve input validator

--- src/main/kotlin/api/controllers/Validator.kt
+++ src/main/kotlin/api/controllers/Validator.kt
@@ -3,16 +3,27 @@ package api.controllers
 import java.util.regex.Pattern
 
 class Validator {
-    fun containsNumber(toCheck: String): Boolean {
+    private val specialCharacterPattern = Pattern.compile("[!@#$%&*()_+=|<>?{}\\[\\]~-]", Pattern.CASE_INSENSITIVE)
+
+    fun containsNumbers(toCheck: String?): Boolean {
+        if (toCheck == null) {
+            return false
+        }
         return toCheck.any { Character.isDigit(it) }
     }
 
-    fun containsSpecialCharacter(str: String?): Boolean {
-        if (str == null) {
+    fun isAllNumbers(toCheck: String?): Boolean {
+        if (toCheck == null) {
             return false
         }
-        val pattern = Pattern.compile("[!@#$%&*()_+=|<>?{}\\[\\]~-]", Pattern.CASE_INSENSITIVE)
-        val matcher = pattern.matcher(str)
+        return toCheck.all { Character.isDigit(it) }
+    }
+
+    fun containsSpecialCharacter(toCheck: String?): Boolean {
+        if (toCheck == null) {
+            return false
+        }
+        val matcher = specialCharacterPattern.matcher(toCheck)
         return matcher.find()
     }
 }
