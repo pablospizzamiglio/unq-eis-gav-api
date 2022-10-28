f81ed3457aeaa06ab263c51f769cd34ee2b40f11
Gabriel <g.r.maugeri@gmil.com>
2022-10-27 22:12:51 -0300

    Correción de bugs

--- src/main/kotlin/api/controllers/Validator.kt
+++ src/main/kotlin/api/controllers/Validator.kt
@@ -17,4 +17,20 @@ class Validator {
         val mSpecial: Matcher = special.matcher(str)
         return mSpecial.matches()
     }
+
+    fun isValidEMail(eMail: String?): Boolean{
+        //val regExp = "^[A-Za-z](.*)([@]{1})(.{1,})(\\.)(.{1,})"
+        //return regExp.toRegex().matches(eMail as CharSequence)
+        return Pattern.compile("^(([\\w-]+\\.)+[\\w-]+|([a-zA-Z]|[\\w-]{2,}))@"
+                    + "((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                    + "[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\."
+                    + "([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                    + "[0-9]{1,2}|25[0-5]|2[0-4][0-9]))|"
+                    + "([a-zA-Z]+[\\w-]+\\.)+[a-zA-Z]{2,4})$"
+        ).matcher(eMail).matches()
+    }
+
+    fun isValidUserType(type: String?): Boolean {
+        return type == "CLIENT" || type == "ASSISTENCE"
+    }
 }
--- src/test/kotlin/services/UserServiceTest.kt
+++ src/test/kotlin/services/UserServiceTest.kt
@@ -53,4 +53,137 @@ class UserServiceTest {
 
         assertTrue { users.isEmpty() }
     }
+
+    @Test
+    fun firstNameWithNumbersRiseERROR() {
+        val user = UserCreateRequestDTO(
+            "Lu1s",
+            "Bello",
+            "CLIENT",
+            "luisbello@email.com",
+            "5555555555"
+        )
+
+        assertThrows<RuntimeException> { userService.createUser(user) }
+
+        val users = userService.findAll()
+
+        assertTrue { users.isEmpty() }
+    }
+
+    @Test
+    fun lastNameWithSpecialsCharsRiseERROR() {
+        val user = UserCreateRequestDTO(
+            "Luis",
+            "Bell++o",
+            "CLIENT",
+            "luisbello@email.com",
+            "5555555555"
+        )
+
+        assertThrows<RuntimeException> { userService.createUser(user) }
+
+        val users = userService.findAll()
+
+        assertTrue { users.isEmpty() }
+    }
+
+    @Test
+    fun lastNameWithNumbersRiseERROR() {
+        val user = UserCreateRequestDTO(
+            "Luis",
+            "Bell05",
+            "CLIENT",
+            "luisbello@email.com",
+            "5555555555"
+        )
+
+        assertThrows<RuntimeException> { userService.createUser(user) }
+
+        val users = userService.findAll()
+
+        assertTrue { users.isEmpty() }
+    }
+
+    @Test
+    fun invalidEMailERROR_1() {
+        val user = UserCreateRequestDTO(
+            "Luis",
+            "Bello",
+            "CLIENT",
+            "luis@bello@email.com",
+            "5555555555"
+        )
+
+        assertThrows<RuntimeException> { userService.createUser(user) }
+
+        val users = userService.findAll()
+
+        assertTrue { users.isEmpty() }
+    }
+
+    @Test
+    fun invalidEMailERROR_2() {
+        val user = UserCreateRequestDTO(
+            "Luis",
+            "Bello",
+            "CLIENT",
+            "luisbello@email",
+            "5555555555"
+        )
+
+        assertThrows<RuntimeException> { userService.createUser(user) }
+
+        val users = userService.findAll()
+
+        assertTrue { users.isEmpty() }
+    }
+    @Test
+    fun invalidEMailERROR_3() {
+        val user = UserCreateRequestDTO(
+            "Luis",
+            "Bello",
+            "CLIENT",
+            "luisbello.email.com",
+            "5555555555"
+        )
+
+        assertThrows<RuntimeException> { userService.createUser(user) }
+
+        val users = userService.findAll()
+
+        assertTrue { users.isEmpty() }
+    }
+
+    @Test
+    fun inavlidUserType_ERROR() {
+        val user = UserCreateRequestDTO(
+            "Luis",
+            "Bello",
+            "CLIE",
+            "luisbello.email.com",
+            "5555555555"
+        )
+
+        assertThrows<RuntimeException> { userService.createUser(user) }
+
+        val users = userService.findAll()
+
+        assertTrue { users.isEmpty() }
+    }
+    @Test
+    fun saveAValidUser() {
+        val user = UserCreateRequestDTO(
+            "Luis",
+            "Bello",
+            "CLIENT",
+            "luisbello@email.com",
+            "5555555555"
+        )
+
+        userService.createUser(user)
+        val users = userService.findAll()
+
+        assertTrue { users.size == 1 }
+    }
 }
