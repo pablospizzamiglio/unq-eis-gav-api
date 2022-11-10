f7876a24c50c2735b4d9a9fc5d15619c1e0d9d77
pablospizzamiglio <spizzamiglio.pablo@gmail.com>
2022-11-09 23:21:50 -0300

    Add cancellation cost to the /assistance endpoint

--- src/main/kotlin/api/dtos/AssistanceSimpleDTO.kt
+++ src/main/kotlin/api/dtos/AssistanceSimpleDTO.kt
@@ -1,24 +1,27 @@
 package api.dtos
 
+import entity.Assistance
 import entity.User
 import java.util.*
 
 class AssistanceSimpleDTO(
     val id: UUID,
+    val assistant: User,
     val kind: String,
     val fixedCost: Double,
     val costPerKm: Double,
-    val assistant: User
+    val cancellationCost: Double,
 ) {
     companion object {
-        fun fromModel(
-            id: UUID?,
-            kind: String,
-            fixedCost: Double,
-            costPerKm: Double,
-            assistant: User
-        ): AssistanceSimpleDTO {
-            return AssistanceSimpleDTO(id!!, kind, fixedCost, costPerKm, assistant)
+        fun fromModel(assistance: Assistance): AssistanceSimpleDTO {
+            return AssistanceSimpleDTO(
+                assistance.id!!,
+                assistance.user,
+                assistance.kind.toString(),
+                assistance.fixedCost,
+                assistance.costPerKm,
+                assistance.cancellationCost
+            )
         }
     }
 }
