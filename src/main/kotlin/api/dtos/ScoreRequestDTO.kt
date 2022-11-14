5dfcf76d3ec66c0afd5114990984553293e9e9a1
pablospizzamiglio <spizzamiglio.pablo@gmail.com>
2022-11-14 00:48:42 -0300

    Remove unused code

--- src/main/kotlin/api/dtos/ScoreRequestDTO.kt
+++ src/main/kotlin/api/dtos/ScoreRequestDTO.kt
@@ -3,21 +3,7 @@ package api.dtos
 import java.util.*
 
 class ScoreRequestDTO(
-    val orderId: UUID,
-    val userId: UUID,
-    val score: Int,
-) {
-    companion object {
-        fun fromModel(
-            orderId: UUID,
-            userId: UUID,
-            score: Int
-        ): ScoreRequestDTO {
-            return ScoreRequestDTO(
-                orderId,
-                userId,
-                score
-            )
-        }
-    }
-}
+    val orderId: UUID?,
+    val userId: UUID?,
+    val score: Int?,
+)
